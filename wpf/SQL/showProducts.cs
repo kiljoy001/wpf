bc0f363a7caf2687a51ba1aa93e662fd583d8b37
sguyton <sguyton@J110-08>
2017-03-01 08:53:58 -0800

    added more to the try  block to get all records from the  products table and updated table with new column that is boolean

--- wpf/SQL/showProducts.cs
+++ wpf/SQL/showProducts.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace wpf.SQL
 {
@@ -11,7 +12,7 @@ namespace wpf.SQL
     {
         public showProducts()
         {
-            string selectData = "[product_name], [product_units] ,[product_price] FROM[dbo].[Product]";
+            string selectData = "[product_name], [product_units] ,[product_price], [product_enable] FROM[dbo].[Product]";
             using (SqlConnection dbConnect = new SqlConnection())
             {
                 dbConnect.ConnectionString = connection;
@@ -22,6 +23,8 @@ namespace wpf.SQL
                 try
                 {
                     selectAll.Connection = dbConnect;
+                    selectAll.CommandType = CommandType.Text;
+                    selectAll.CommandText = selectData;
                 }
                 catch
                 {
